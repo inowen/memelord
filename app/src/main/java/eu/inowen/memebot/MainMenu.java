73e24de1521e6ba19d59fe307fbc667773477099
inowen <inoah@correo.ugr.es>
2020-10-12 16:56:07 +0200

    Suggestions: Make button open new (empty) activity.

--- app/src/main/java/eu/inowen/memebot/MainMenu.java
+++ app/src/main/java/eu/inowen/memebot/MainMenu.java
@@ -2,13 +2,29 @@ package eu.inowen.memebot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    private Button suggestionsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+        // Enable suggestions button to show suggestions screen
+        suggestionsButton = findViewById(R.id.suggestions_btn);
+        suggestionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SuggestionsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

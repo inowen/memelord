4399ef9bbd6c826f294aaf4f5614b0d530f107ae
inowen <inoah@correo.ugr.es>
2021-02-01 15:26:47 +0100

    Testing: Try doing network operations on different thread

--- app/src/main/java/eu/inowen/app/testing/TestingActivity.java
+++ app/src/main/java/eu/inowen/app/testing/TestingActivity.java
@@ -17,9 +17,30 @@ public class TestingActivity extends AppCompatActivity {
 
         TextView testDisplay = findViewById(R.id.testDisplay);
 
-        String url = "https://i.redd.it/n4k60ag3yre61.jpg";
-        ImageDownloader downloader = new ImageDownloader(url, "/home/user");
-        String text = "Identifier: " + downloader.extractIdentifier(url) + "  Extension: " + downloader.getExtension(url);
-        testDisplay.setText(text);
+        String url = "https://i.redd.it/vssspzxvdue61.jpg";
+        ImageDownloader downloader = new ImageDownloader(url, "data/data/eu.inowen.memebot/cache");
+
+        Test test = new Test(downloader);
+        Thread t = new Thread(test);
+        while (test.getResponse() == "") { }
+        testDisplay.setText("Download response: " + test.getResponse());
+
     }
+}
+
+class Test implements Runnable {
+
+    private ImageDownloader imageDownloader;
+    private String response = "";
+
+    public Test(ImageDownloader im) {
+        imageDownloader = im;
+    }
+
+    @Override
+    public void run() {
+        response = imageDownloader.download();
+    }
+
+    public String getResponse() { return response; }
 }
--- app/src/main/java/eu/inowen/app/utils/ImageDownloader.java
+++ app/src/main/java/eu/inowen/app/utils/ImageDownloader.java
@@ -1,6 +1,12 @@
 package eu.inowen.app.utils;
 
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
 /**
  * Helper to download something from the web via its url.
  */
@@ -21,10 +27,33 @@ public class ImageDownloader {
 
     /**
      * Download the image
-     * @return
+     * @return Path to the name of the image in the filesystem
      */
     public String download() {
-        throw new UnsupportedOperationException("Not implemented");
+        try {
+            URL url = new URL(urlStr);
+            InputStream in = new BufferedInputStream(url.openStream());
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buf = new byte[1024];
+            int n = 0;
+            while (-1!=(n=in.read(buf))) {
+                out.write(buf, 0, n);
+            }
+            out.close();
+            in.close();
+            byte[] response = out.toByteArray();
+
+            FileOutputStream fos = new FileOutputStream(destination);
+            fos.write(response);
+            fos.close();
+        }
+        catch (Exception e) {
+            System.err.println("Exception: " + e.toString());
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        return destination;
     }
 
     /**
@@ -32,7 +61,7 @@ public class ImageDownloader {
      * @param url The url with an extension at the end
      * @return Extension
      */
-    public String getExtension(String url) {
+    private String getExtension(String url) {
         String[] split = url.split("\\.");
         int length = split.length;
         if (length > 1) {
@@ -41,7 +70,13 @@ public class ImageDownloader {
         return "";
     }
 
-    public String extractIdentifier(String url) {
+    /**
+     * Takes a unique identifier from a url, to temporarily
+     * store an image with that name without collisions.
+     * @param url
+     * @return
+     */
+    private String extractIdentifier(String url) {
         String[] split = url.split("/");
         if (split.length > 1) {
             return split[split.length-1].split("\\.")[0];

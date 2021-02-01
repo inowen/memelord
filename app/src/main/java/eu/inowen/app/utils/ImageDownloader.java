1eb9c0126901e53648e0bbd75c913ec5d00c14aa
inowen <inoah@correo.ugr.es>
2021-02-01 10:09:08 +0100

    Utils: Add ImageDownloader class
    Downloads images from the web via their url and saves them on disk

---
+++ app/src/main/java/eu/inowen/app/utils/ImageDownloader.java
@@ -0,0 +1,27 @@
+package eu.inowen.app.utils;
+
+
+/**
+ * - Take a url name through the constructor.
+ *   - Set a path for download, giving the image some unique name f(url)
+ *   - Add the extension (jpg, png, ...)
+ *
+ * - Download method: Downloads the image and returns the path where the image was stored.
+ */
+public class ImageDownloader {
+
+    private String urlStr;
+    private String destination;
+
+    /**
+     * 
+     * @param url
+     */
+    public ImageDownloader(String url) {
+
+    }
+
+    public String download() {
+
+    }
+}

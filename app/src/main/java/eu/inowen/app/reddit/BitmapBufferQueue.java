4bc504c049f46cd615cc0bb452c2a23b852e02cf
inowen <inoah@correo.ugr.es>
2021-02-06 20:29:27 +0100

    Implement BitmapBufferQueue

--- app/src/main/java/eu/inowen/app/reddit/BitmapBufferQueue.java
+++ app/src/main/java/eu/inowen/app/reddit/BitmapBufferQueue.java
@@ -1,6 +1,17 @@
 package eu.inowen.app.reddit;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Queue;
+
+import eu.inowen.app.R;
+import eu.inowen.app.gui.MainMenu;
+import eu.inowen.app.utils.ImageDownloader;
 
 /**
  * Takes a SubredditIterator and a desired size for the cache queue.
@@ -9,34 +20,70 @@ import android.graphics.Bitmap;
 public class BitmapBufferQueue {
 
     private SubredditIterator subredditIterator;
+    private Queue<Bitmap> buffer;
     private int maxSize;
     private int halfSize;
     private Thread downloaderThread;
-    private static Bitmap noMoreImages;
-
-    static {
-
-    }
+    private Bitmap noMoreImages;
 
     /**
      * A BitmapBufferQueue downloads images from the posts in a subreddit whose urls can
      * be decoded into images, and stores them in a buffer.
      * @param it Iterator over a subreddit.
      * @param desiredSize How long the cache queue can become.
+     * @param applicationContext Context object, only used to access resources
      */
-    public BitmapBufferQueue(SubredditIterator it, int desiredSize) {
+    public BitmapBufferQueue(SubredditIterator it, int desiredSize, Context applicationContext) {
         subredditIterator = it;
         maxSize = Math.max(desiredSize, 2);
         halfSize = maxSize/2;
+        noMoreImages = BitmapFactory.decodeResource(applicationContext.getResources(), R.drawable.end_of_buffer);
         // Create the thread that fills the cache
+        downloaderThread = new Thread(new RefillCache());
+        downloaderThread.start();
     }
 
+    /**
+     * The next bitmap in the buffer (or null if empty).
+     * @return Bitmap
+     */
+    public synchronized Bitmap next() { // Make this launch the refilling thread?
+        if (size()<halfSize) {
+            if (!downloaderThread.isAlive()) {
+                downloaderThread = new Thread(new RefillCache());
+            }
+        }
+        return buffer.poll();
+    }
 
+    /**
+     * Number of elements currently in the buffer.
+     * @return int
+     */
+    public synchronized int size() {
+        return buffer.size();
+    }
 
-    public class RefillCache implements Runnable {
+    // Add an element to the bitmap buffer
+    private synchronized void addToBuffer(Bitmap bitmap) {
+        buffer.add(bitmap);
+    }
+
+    // The downloading thread runs this to refill the queue.
+    private class RefillCache implements Runnable {
         @Override
         public void run() {
-
+            while(size() < maxSize) {
+                // Download image as bitmap
+                ImageDownloader downloader = new ImageDownloader(subredditIterator.nextUrl(), new File(""));
+                Bitmap bm = null;
+                try { bm = downloader.downloadBitmap(); } catch (IOException e) { e.printStackTrace(); }
+                // Add to buffer
+                if (bm != null) {
+                    addToBuffer(bm);
+                }
+            }
+            System.out.println("Debug: Refill thread finished");
         }
     }
 }

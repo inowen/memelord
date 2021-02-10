af5d72e5c8d94cab57c5af0e13151f2e6ca2464b
inowen <inoah@correo.ugr.es>
2021-02-10 21:38:30 +0100

    Testing: Scrolling view on LinearLayout with added buttons
    (still isn't working but tested it)

--- app/src/main/java/eu/inowen/app/testing/TestingActivity.java
+++ app/src/main/java/eu/inowen/app/testing/TestingActivity.java
@@ -1,13 +1,21 @@
 package eu.inowen.app.testing;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import java.io.Serializable;
+
 import eu.inowen.app.R;
+import eu.inowen.app.gui.ImageScrollActivity;
 import eu.inowen.app.gui.ImageViewPagerAdapter;
 import eu.inowen.app.reddit.ListingCategory;
 import eu.inowen.app.reddit.RequestSpecification;
@@ -27,5 +35,23 @@ public class TestingActivity extends AppCompatActivity {
         // The ImageView to test showing images
         final ImageView testImageView = findViewById(R.id.testImageView);
 
+        // TESTING ADDING STUFF TO HORIZONTAL SCROLL VIEW
+        HorizontalScrollView hsv = findViewById(R.id.test_horizontal_scroll_view);
+
+
+    }
+
+    class MyOnClickListener implements View.OnClickListener {
+        private RequestSpecification requestSpecification;
+        public MyOnClickListener(RequestSpecification requestSpecification) {
+            this.requestSpecification = requestSpecification;
+        }
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), ImageScrollActivity.class);
+            intent.putExtra(RequestSpecification.class.getSimpleName(), requestSpecification);
+            startActivity(intent);
+            finish();
+        }
     }
 }
